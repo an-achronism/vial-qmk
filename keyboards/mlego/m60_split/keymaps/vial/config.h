9542a294b15fa9b4945ceed4ee81a795df12877d
an_achronism <87213873+an-achronism@users.noreply.github.com>
2022-06-12 20:55:15 +0100

    mlego/m60_split ported, compiles successfully

---
+++ keyboards/mlego/m60_split/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define TAPPING_TOGGLE 2
+#define UNICODE_SELECTED_MODES UC_LNX, UC_MAC, UC_WINC
+#define VIAL_KEYBOARD_UID {0xC1, 0xB2, 0x7F, 0x95, 0x8A, 0xE5, 0x94, 0xA7}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 9, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5, 0 }
