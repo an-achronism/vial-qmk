f34f846f615652a6d063535b91e3e70b3958caf0
Cipulot <40441626+Cipulot@users.noreply.github.com>
2022-07-26 02:44:20 +0200

    Added suspad

---
+++ keyboards/cipulot/suspad/config.h
@@ -0,0 +1,38 @@
+/* Copyright 2022 Cipulot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 3
+
+#define MATRIX_ROW_PINS { B6, B5, B8, B9 }
+#define MATRIX_COL_PINS { B0, B4, B7 }
+
+/* COL2ROW, ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
---
+++ keyboards/cipulot/suspad/readme.md
@@ -0,0 +1,19 @@
+# SUSPad
+
+ඞ
+
+* Keyboard Maintainer: [cipulot](https://github.com/cipulot)
+* Hardware Supported: SUSPad
+* Hardware Availability: LOL nice try buddy
+
+Make example for this keyboard (after setting up your build environment):
+
+    make suspad:default
+    make suspad:via
+
+Flashing example for this keyboard:
+
+    make suspad:default:flash
+    make suspad:via:flash
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
